db85186ee7b3c6f45dc3d994be2bc79961e2784d
Deepkumar0047 <deepk636328@gmail.com>
2026-07-19 10:31:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/BST to max heap/bst-to-max-heap.cpp
@@ -0,0 +1,37 @@
+/*struct Node {
+    int data;
+    Node *left, *right;
+};*/
+
+class Solution {
+public:
+
+    void inorder(Node* root, vector<int>& ans) {
+        if (root == NULL)
+            return;
+
+        inorder(root->left, ans);
+        ans.push_back(root->data);
+        inorder(root->right, ans);
+    }
+
+    void postorder(Node* root, vector<int>& ans, int &index) {
+        if (root == NULL)
+            return;
+
+        postorder(root->left, ans, index);
+        postorder(root->right, ans, index);
+
+        root->data = ans[index];
+        index++;
+    }
+
+    void convertToMaxHeapUtil(Node* root) {
+        vector<int> ans;
+
+        inorder(root, ans);
+
+        int index = 0;
+        postorder(root, ans, index);
+    }
+};
