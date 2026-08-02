58ab35f2f6c01e2a59f45aa6875dbf5fca46a582
Deepkumar0047 <deepk636328@gmail.com>
2026-08-02 09:31:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Prime Number/prime-number.cpp
+++ Difficulty: Easy/Prime Number/prime-number.cpp
@@ -1,14 +1,15 @@
 class Solution {
-  public:
+public:
     bool isPrime(int n) {
-        if(n<=1)
-        return false;
-        
-        for(int i=2;i*i<=n;i++){
-            if(n%i==0)
+
+        if (n <= 1)
             return false;
+
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0)
+                return false;
         }
+
         return true;
-        
     }
 };
