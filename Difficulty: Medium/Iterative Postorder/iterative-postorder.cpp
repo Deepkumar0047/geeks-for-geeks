fbaf8a07eb8b269456f6aa1916f062b5dc8311f5
Deepkumar0047 <deepk636328@gmail.com>
2026-08-07 21:46:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Iterative Postorder/iterative-postorder.cpp
@@ -0,0 +1,39 @@
+/* Tree Node
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};*/
+class Solution {
+  public:
+    vector<int> postOrder(Node* root) {
+        vector<int> ans;
+
+        if (root == NULL)
+            return ans;
+
+        stack<Node*> st1, st2;
+
+        st1.push(root);
+
+        while (!st1.empty()) {
+            Node* curr = st1.top();
+            st1.pop();
+
+            st2.push(curr);
+
+            if (curr->left)
+                st1.push(curr->left);
+
+            if (curr->right)
+                st1.push(curr->right);
+        }
+
+        while (!st2.empty()) {
+            ans.push_back(st2.top()->data);
+            st2.pop();
+        }
+
+        return ans;
+    }
+};
